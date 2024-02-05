74b8b148ba226298a0173a0c9bc2c3380fe603a6
Jia Wei <jiawei.teo@hotmail.com>
2024-02-06 00:10:57 +0800

    Add sample rabbitmq

---
+++ worker/receive.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import pika, sys, os, time
+
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='acfirstExchange', exchange_type='fanout')
+
+    result = channel.queue_declare(queue='acfirstQueue', exclusive=True)
+    queue_name = result.method.queue
+    print(queue_name)
+
+    channel.queue_bind(exchange='acfirstExchange', queue=queue_name)
+
+    print(' [*] Waiting for logs. To exit press CTRL+C')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] {body}")
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
