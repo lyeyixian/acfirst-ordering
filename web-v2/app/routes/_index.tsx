ac23eb5aa736f042cb9f83fdbe794124e40a3280
Lye Yi Xian <lyeyx999@hotmail.com>
2024-03-24 00:03:41 +0800

    Fix eslint issue

--- web-v2/app/routes/_index.tsx
+++ web-v2/app/routes/_index.tsx
@@ -1,13 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Welcome } from "~/components/Welcome/Welcome";
-import { ColorSchemeToggle } from "~/components/ColorSchemeToggle/ColorSchemeToggle";
+import type { MetaFunction } from '@remix-run/node'
+import { Welcome } from '~/components/Welcome/Welcome'
+import { ColorSchemeToggle } from '~/components/ColorSchemeToggle/ColorSchemeToggle'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Mantine Remix App" },
-    { name: "description", content: "Welcome to Mantine!" },
-  ];
-};
+    { title: 'Mantine Remix App' },
+    { name: 'description', content: 'Welcome to Mantine!' },
+  ]
+}
 
 export default function Index() {
   return (
@@ -15,5 +15,5 @@ export default function Index() {
       <Welcome />
       <ColorSchemeToggle />
     </div>
-  );
+  )
 }
