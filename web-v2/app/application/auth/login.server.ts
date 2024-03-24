e2889db107d3f05b7dfc5fa388f2973ff8d4bffb
Lye Yi Xian <lyeyx999@hotmail.com>
2024-03-24 17:20:47 +0800

    Remove type

--- web-v2/app/application/auth/login.server.ts
+++ web-v2/app/application/auth/login.server.ts
@@ -1,9 +1,7 @@
-import type { ActionFunction, ActionFunctionArgs } from '@remix-run/node'
+import type { ActionFunction } from '@remix-run/node'
 import { createUserSession } from '~/session.server'
 
-export const loginAction: ActionFunction = async ({
-  request,
-}: ActionFunctionArgs) => {
+export const loginAction: ActionFunction = async ({ request }) => {
   const form = await request.formData()
   const idToken = form.get('idToken')?.toString() || ''
 
