c1c574ae0441308315335d74da4d85dd956baaa0
Lye Yi Xian <lyeyx999@hotmail.com>
2024-03-24 22:04:08 +0800

    Remove color scheme from template

--- web-v2/app/root.tsx
+++ web-v2/app/root.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css'
 
-import { MantineProvider, ColorSchemeScript } from '@mantine/core'
+import { MantineProvider } from '@mantine/core'
 import {
   Links,
   Meta,
@@ -55,7 +55,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <ColorSchemeScript />
       </head>
       <body>
         <MantineProvider>{children}</MantineProvider>
