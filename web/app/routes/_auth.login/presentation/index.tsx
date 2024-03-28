18a73cfc51c30a3df7cdd2a6ef5604004c7deada
Lye Yi Xian <lyeyx999@hotmail.com>
2024-03-29 01:19:25 +0800

    Move into .client folder

--- web/app/routes/_auth.login/presentation/index.tsx
+++ web/app/routes/_auth.login/presentation/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@remix-run/react'
-import { useLogin } from '~/hooks/auth/hooks'
+import { useLogin } from '~/.client/hooks/auth/hooks'
 
 export default function LoginPage() {
   const { handleSubmit } = useLogin()
