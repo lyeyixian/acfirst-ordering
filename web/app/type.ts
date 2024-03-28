c1b52ee5ae8588c2d8e8941e52dbd4214c281f97
Lye Yi Xian <lyeyx999@hotmail.com>
2024-03-29 01:04:46 +0800

    Reorder type

--- web/app/type.ts
+++ web/app/type.ts
@@ -1,42 +1,22 @@
 import { Timestamp } from '@google-cloud/firestore'
 
-export enum EventType {
-  REFRESH_STOCKS = 'refreshStocks',
-  CREATE_INVOICE = 'createInvoice',
-  CREATE_DELIVERY_ORDER = 'createDeliveryOrder',
-}
-
 export interface CallbackData {
   data?: Record<string, unknown>
   error?: string
   status: number
 }
+
 export interface ResponseData {
   user: User
   data?: Record<string, unknown>
   error?: string
 }
 
-export interface CreateUserPayload {
-  username: string
-  email: string
-  company: string
-  userId: string
-}
-export interface User {
-  username: string
-  email: string
-  company: string
-  userId: string
-}
-
-export interface Stock {
-  itemCode: string
-  location: string
-  batch: string
-  quantity: number
-  pricePerUnit: number
-  updatedAt: Timestamp
+// Events
+export enum EventType {
+  REFRESH_STOCKS = 'refreshStocks',
+  CREATE_INVOICE = 'createInvoice',
+  CREATE_DELIVERY_ORDER = 'createDeliveryOrder',
 }
 
 export interface Event {
@@ -48,18 +28,46 @@ export interface Event {
   createdAt: Timestamp
   updatedAt: Timestamp
 }
+
 export enum EventStatus {
   QUEUED = 'queued',
   PROCESSING = 'processing',
   COMPLETED = 'completed',
   FAILED = 'failed',
 }
+
 export interface EventPayload {
   docNo: string
   code: string
   data: EventStockPayload[]
 }
+
 export interface EventStockPayload {
   itemCode: string
   qty: number
+}
+
+// Users
+export interface CreateUserPayload {
+  username: string
+  email: string
+  company: string
+  userId: string
+}
+
+export interface User {
+  username: string
+  email: string
+  company: string
+  userId: string
+}
+
+// Stocks
+export interface Stock {
+  itemCode: string
+  location: string
+  batch: string
+  quantity: number
+  pricePerUnit: number
+  updatedAt: Timestamp
 }
