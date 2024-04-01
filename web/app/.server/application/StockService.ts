a9df9829e9255d2c8a53b58d8fd8ff0ba5142ce2
Lye Yi Xian <lyeyx999@hotmail.com>
2024-04-01 22:12:11 +0800

    Add getStock and refreshStocks

--- web/app/.server/application/StockService.ts
+++ web/app/.server/application/StockService.ts
@@ -1,8 +1,12 @@
-import { Stock } from '~/common/type'
+import { Event, EventStatus, EventType, Stock, User } from '~/common/type'
 import { db } from '../infrastructure/firebase'
+import { Timestamp } from 'firebase-admin/firestore'
+import { eventService } from './EventService'
 
 export interface IStockService {
   getStocks: () => Promise<Stock[]>
+  getStock: (itemCode: string) => Promise<Stock>
+  refreshStocks: (user: User) => Promise<void>
 }
 
 async function getStocks() {
@@ -19,6 +23,30 @@ async function getStocks() {
   return data
 }
 
+async function getStock(itemCode: string) {
+  const docSnapshot = await db.collection('stocks').doc(itemCode).get()
+
+  if (!docSnapshot.exists) {
+    throw new Error('Stock does not exist')
+  }
+
+  return docSnapshot.data() as Stock
+}
+
+async function refreshStocks(user: User) {
+  const event: Event = {
+    type: EventType.REFRESH_STOCKS,
+    status: EventStatus.QUEUED,
+    createdBy: user.username,
+    createdAt: Timestamp.now(),
+    updatedAt: Timestamp.now(),
+  }
+
+  await eventService.createEvent(event)
+}
+
 export const stockService: IStockService = {
   getStocks,
+  getStock,
+  refreshStocks,
 }
