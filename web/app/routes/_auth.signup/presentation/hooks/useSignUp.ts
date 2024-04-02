0f8441ef7c65eaabda848049436ace9f9c199d62
Lye Yi Xian <lyeyx999@hotmail.com>
2024-04-02 22:24:32 +0800

    Remove fetcher from dependency array

--- web/app/routes/_auth.signup/presentation/hooks/useSignUp.ts
+++ web/app/routes/_auth.signup/presentation/hooks/useSignUp.ts
@@ -13,41 +13,38 @@ export function useSignUp() {
   const fetcher = useFetcher()
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleSubmit = useCallback(
-    async (e: SyntheticEvent) => {
-      e.preventDefault()
+  const handleSubmit = useCallback(async (e: SyntheticEvent) => {
+    e.preventDefault()
 
-      const target: SignUpFormTarget = e.target
-      const email = target.email?.value || ''
-      const password = target.password?.value || ''
-      const company = target.company?.value || ''
-      const username = target.username?.value || ''
+    const target: SignUpFormTarget = e.target
+    const email = target.email?.value || ''
+    const password = target.password?.value || ''
+    const company = target.company?.value || ''
+    const username = target.username?.value || ''
 
-      try {
-        console.log('creating user ...')
+    try {
+      console.log('creating user ...')
 
-        setIsLoading(true)
+      setIsLoading(true)
 
-        const credential = await signUp(email, password)
+      const credential = await signUp(email, password)
 
-        console.log('user created')
+      console.log('user created')
 
-        const user = credential.user
-        const idToken = await user.getIdToken()
-        const userEmail = user.email
-        const uid = user.uid
+      const user = credential.user
+      const idToken = await user.getIdToken()
+      const userEmail = user.email
+      const uid = user.uid
 
-        // Trigger a POST request which the action will handle
-        fetcher.submit(
-          { idToken, email: userEmail, uid, company, username },
-          { method: 'post', action: '/signup' }
-        )
-      } catch (e: unknown) {
-        console.error(e)
-      }
-    },
-    [fetcher]
-  )
+      // Trigger a POST request which the action will handle
+      fetcher.submit(
+        { idToken, email: userEmail, uid, company, username },
+        { method: 'post', action: '/signup' }
+      )
+    } catch (e: unknown) {
+      console.error(e)
+    }
+  }, [])
 
   useEffect(() => {
     setIsLoading(fetcher.state !== 'idle')
