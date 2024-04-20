fd2d5820cc9bd5e0119723b3dc4b34cd5ed8be0d
Lye Yi Xian <lyeyx999@hotmail.com>
2024-04-21 01:31:05 +0800

    Refactor NavBar

---
+++ web/app/routes/_index/presentation/components/HeaderTabs/index.tsx
@@ -0,0 +1,77 @@
+import cx from 'clsx'
+import { useState } from 'react'
+import {
+  Avatar,
+  UnstyledButton,
+  Group,
+  Text,
+  Menu,
+  rem,
+  Image,
+  Container,
+} from '@mantine/core'
+import { IconLogout, IconChevronDown } from '@tabler/icons-react'
+import classes from './HeaderTabs.module.css'
+import { useUser } from '~/components/hooks/user'
+import UserInfo from '../UserInfo'
+import { Link } from '@remix-run/react'
+
+export default function HeaderTabs() {
+  const [userMenuOpened, setUserMenuOpened] = useState(false)
+  const { company } = useUser()
+
+  return (
+    <Container>
+      <Group h="100%" justify="space-between">
+        <Image w={60} src="/nav-logo.svg" />
+
+        <Menu
+          width={260}
+          position="bottom-end"
+          transitionProps={{ transition: 'pop-top-right' }}
+          onClose={() => setUserMenuOpened(false)}
+          onOpen={() => setUserMenuOpened(true)}
+          withinPortal
+        >
+          <Menu.Target>
+            <UnstyledButton
+              className={cx(classes.user, {
+                [classes.userActive]: userMenuOpened,
+              })}
+            >
+              <Group gap={7}>
+                <Avatar radius="xl" size="md" />
+                <Text fw={500} size="sm" lh={1} mr={3}>
+                  {company}
+                </Text>
+                <IconChevronDown
+                  style={{ width: rem(12), height: rem(12) }}
+                  stroke={1.5}
+                />
+              </Group>
+            </UnstyledButton>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item component={UserInfo}></Menu.Item>
+
+            <Menu.Divider />
+
+            <Menu.Item
+              color="red"
+              leftSection={
+                <IconLogout
+                  style={{ width: rem(16), height: rem(16) }}
+                  stroke={1.5}
+                />
+              }
+              component={Link}
+              to="/logout"
+            >
+              Log Out
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </Group>
+    </Container>
+  )
+}
--- web/app/routes/_index/presentation/index.tsx
+++ web/app/routes/_index/presentation/index.tsx
@@ -3,35 +3,28 @@ import { StocksTable } from './components/StocksTable'
 import { OrderHistory } from './components/OrderHistory'
 import { homeLoader } from '../application/loader.server'
 import { AppShell, Space } from '@mantine/core'
-import { Navbar } from '~/components/Navbar/Navbar'
 import RouterTransition from '~/components/RouterTransition'
-import { useDisclosure } from '@mantine/hooks'
+import HeaderTabs from './components/HeaderTabs'
 
 export default function HomePage() {
   const loaderData = useLoaderData<typeof homeLoader>()
   const { stocks, events } = loaderData.data
-  const [opened, { toggle }] = useDisclosure();
 
   return (
-    <AppShell padding={0} navbar={{
-      width: 300,
-      breakpoint: 'sm',
-      collapsed: { mobile: !opened },
-    }}>
-      <AppShell.Navbar p="md">
-        <Navbar/>
-      </AppShell.Navbar>
+    <AppShell header={{ height: 56 }} padding="md">
+      <AppShell.Header>
+        <HeaderTabs />
+      </AppShell.Header>
 
-      <AppShell.Main>      
+      <AppShell.Main>
         <RouterTransition />
-          <div className="remix__page">
-            <Space h="md" />
-            <OrderHistory orderHistories={events} />
-            <Space h="md" />
-            <StocksTable stocks={stocks} />
-          </div>
-        </AppShell.Main>
+        <div className="remix__page">
+          <Space h="md" />
+          <OrderHistory orderHistories={events} />
+          <Space h="md" />
+          <StocksTable stocks={stocks} />
+        </div>
+      </AppShell.Main>
     </AppShell>
-
   )
 }
