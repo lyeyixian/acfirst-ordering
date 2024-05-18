1884336c347b02457b084950a328c553e9b12c17
Lye Yi Xian <lyeyx999@hotmail.com>
2024-05-18 23:27:12 +0800

    remove login page background image

--- web/app/routes/_auth/presentation/index.tsx
+++ web/app/routes/_auth/presentation/index.tsx
@@ -1,12 +1,5 @@
-import { BackgroundImage } from '@mantine/core'
 import { Outlet } from '@remix-run/react'
 
 export default function AuthLayout() {
-  return (
-    <div>
-      <BackgroundImage src="/login-page.avif">
-        <Outlet />
-      </BackgroundImage>
-    </div>
-  )
+  return <Outlet />
 }
